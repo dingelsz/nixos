43cd18a43fee8383c13b6dafb65456fd0d60de8e
zach <anon@anon.com>
2025-12-08 09:37:50 -0800

    adding gnome folder

---
+++ components/gnome/default.nix
@@ -0,0 +1,21 @@
+{ lib, ... }:
+
+{
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Packages
+  environment.systemPackages = with pkgs; [
+    gnome.gnome-tweaks
+  ];
+
+  # Dconf Settings
+  lib.hm.gvariant.dconf.settings = with lib.hm.gvariant; {
+    "org/gnome/desktop/input-sources" = {
+      current = mkUint32 0;
+      sources = [ (mkTuple [ "xkb" "us" ]) ];
+      xkb-options = [ "lv3:ralt_switch" "caps:ctrl_modifier" ];
+    };
+  };
+}

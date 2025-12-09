133f380cc032031054bfde5846766e32b1344dff
zach <anon@anon.com>
2025-12-09 13:52:37 -0800

    Adding custom desktop item for browser

--- bases/browser/default.nix
+++ bases/browser/default.nix
@@ -1,10 +1,21 @@
 { config, pkgs, ... }:
 
+
+let
+   browser-desktop = pkgs.makeDesktopItem
+   {
+     name = "browser";
+     desktopName = "browser";
+     exec = "libreworld";
+     icon = "browser";
+   };
+in
 {
-    environment.systemPackages = with pkgs; 
-    [
-      # brave
-      # firefox
-      librewolf
-    ];
-}
+ environment.systemPackages = with pkgs;
+ [
+   # brave
+   # firefox
+  librewolf
+  browser-desktop
+  ];
+ }
